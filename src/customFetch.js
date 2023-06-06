00e5b3b5c2ce8df538272d5b70b9e908ced6ea7c
Adeeb Rohani2 <adeebrohani2@Adeebs-MBP.tampabay.rr.com>
2023-06-06 11:41:39 -0400

    first commit

---
+++ src/customFetch.js
@@ -0,0 +1,10 @@
+// import axios from 'axios';
+
+// const customFetch = axios.create({
+//   baseURL: 'https://api.unsplash.com',
+//   headers: {
+//     Authorization: 'Client-ID pnAaqB0egCfd7gHMKGCCLRoKSc-JhpLf-SdmmDjPEyk',
+//   },
+// });
+
+// export default customFetch;
